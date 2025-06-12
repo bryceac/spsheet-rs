a9571d9ab8a066879611b012c602e33c8156ee76
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-12 10:37:35 -0700

    keep working on getting math right.

--- src/xlsx/read_sheet.rs
+++ src/xlsx/read_sheet.rs
@@ -105,7 +105,9 @@ fn number_to_date(src: &String) -> DateTime<Utc> {
     let num = src.parse::<f64>().unwrap();
     let timestamp = ((((num - num.floor()) * 86400.0) as f64).round()) as i64;
     let hms = DateTime::from_timestamp(timestamp, 0).expect("unable to get datetime");
+    let spreadsheet_epoch = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).expect("Could not establish offset");
 
-    let dt = (Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0) + Duration::days(num.floor() as i64 - 2)).and_hms(hms.hour(), hms.minute(), hms.second());
+
+    let dt = (spreadsheet_epoch + Duration::days(num.floor() as i64 - 2)).and_hms(hms.hour(), hms.minute(), hms.second());
     dt
 }
